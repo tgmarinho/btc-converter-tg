0ad5a18c15ad8506edf89e7ad8a4e560841f83a8
Thiago Marinho <tgmarinho@gmail.com>
2018-01-17 13:45:06 -0300

    iniciando a CLI e tests

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,7 +1,14 @@
-import { expect } from 'chai';
+const expect = require('chai').expect;
+
+const exec = require('child_process').exec;
+const btcConverter = './src/main.js';
 
 describe('Main', () => {
-    it('sould do what...', () => {
-        expect(true).to.be.true;
+    it('should return Hello Word', (done) => {
+        exec(btcConverter, (err, stdout, stderr) => {
+            if(err) throw err;
+            expect(stdout.replace('\n','')).to.be.equal('Hello Word!');
+            done();
+        });        
     });
 });
