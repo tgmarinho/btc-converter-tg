cccb166390770be12aeb7c10facf326dd97e2058
Thiago Marinho <tgmarinho@gmail.com>
2018-01-17 14:49:54 -0300

    conhecendo o commander

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -2,13 +2,14 @@ const expect = require('chai').expect;
 
 const exec = require('child_process').exec;
 const btcConverter = './src/main.js';
+const pkg = require('../package.json');
 
-describe('Main', () => {
-    it('should return Hello Word', (done) => {
-        exec(btcConverter, (err, stdout, stderr) => {
-            if(err) throw err;
-            expect(stdout.replace('\n','')).to.be.equal('Hello Word!');
-            done();
-        });        
+describe('Main CLI', () => {
+  it('should return version of btc-converter', (done) => {
+    exec(`${btcConverter} --version`, (err, stdout, stderr) => {
+      if(err) throw err;
+      expect(stdout.replace('\n', '')).to.be.equal(pkg.version);
+      done();
     });
+  });
 });
